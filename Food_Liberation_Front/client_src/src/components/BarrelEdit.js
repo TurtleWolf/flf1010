5935cf01a029cff1062cc148645d2019b8f63886
TurtleWolfe <jonpohlner@gmail.com>
2018-03-25 13:06:20 +0000

    added React for the front end, completed video number 2

---
+++ Food_Liberation_Front/client_src/src/components/BarrelEdit.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class EditBarrel extends Component {
+
+    addBarrel(newBarrel){
+        console.log(newBarrel);
+        axios.request({
+            method:'post',
+            url:'http://foood-liberation-front-turtlewolfe.c9users.io:8080/api/Barrels',
+            data: newBarrel
+        }).then(response => {
+            this.props.history.push('/');
+        }).catch( err => console.log(err));
+    }
+
+    onSubmit(e){
+        const newBarrel = {
+            Name: this.refs.Name.value,
+            barrel_number: this.refs.barrel_number.value,
+            contents: this.refs.contents.value,
+            date_last_checked: this.refs.date_last_checked.value,
+            date_planted: this.refs.date_planted.value,
+            location: this.refs.location.value,
+            size: this.refs.size.value,
+            notes: this.refs.notes.value
+        }
+        this.addBarrel(newBarrel);
+        e.preventDefault();
+    }
+
+    render () {
+    return (
+            <div className = "container" >
+            <br />
+            <Link className = "btn grey" to = "/">back</Link>
+            <h6>Edit this Barrel</h6>
+            <form onSubmit = {this.onSubmit.bind(this)}>
+                <div className = "input-field" >
+                    <input type = "text" name = "Name" ref = "Name" />
+                    <label htmlFor = "Name" >Name</label>
+                </div>
+                <div className = "input-field" >
+                    <input type = "text" name = "barrel_number" ref = "barrel_number" />
+                    <label htmlFor = "barrel_number" >barrel number</label>
+                </div>
+                <div className = "input-field" >
+                    <input type = "text" name = "contents" ref = "contents" />
+                    <label htmlFor = "contents" >contents</label>
+                </div>
+                <div className = "input-field" >
+                    <input type = "date" name = "date_planted" ref = "date_planted" />
+                    <label htmlFor = "date_planted" ></label>
+                </div>
+                <div className = "input-field" >
+                    <input type = "date" name = "date_last_checked" ref = "date_last_checked" />
+                    <label htmlFor = "date_last_checked" ></label>
+                </div>
+                <div className = "input-field" >
+                    <input type = "text" name = "location" ref = "location" />
+                    <label htmlFor = "location" >location</label>
+                </div>
+                <div className = "input-field" >
+                    <input type = "text" name = "size" ref = "size" />
+                    <label htmlFor = "size" >size</label>
+                </div>
+                <div className = "input-field" >
+                    <input type = "text" name = "notes" ref = "notes" />
+                    <label htmlFor = "notes" >notes</label>
+                </div>                
+                
+                <input type = "submit" value = "Save" className = "btn" />
+            </form>
+            </div>
+           )
+}
+}
+
+export default EditBarrel;
--- Food_Liberation_Front/client_src/src/components/Main.js
+++ Food_Liberation_Front/client_src/src/components/Main.js
@@ -4,12 +4,15 @@ import Barrels from './Barrels';
 import About from './About';
 import BarrelDetails from './BarrelDetails';
 import AddBarrel from './AddBarrel';
+import BarrelEdit from './BarrelEdit';
+
 const Main = () => (
     <main>
     <Switch>
     <Route exact path= '/' component={Barrels} />
     <Route exact path= '/about' component={About} />
     <Route exact path= '/barrels/add' component={AddBarrel} />
+    <Route exact path= '/barrels/edit/:id' component={BarrelEdit} />
     <Route exact path= '/barrels/:id' component={BarrelDetails} />
     </Switch>
     </main>
