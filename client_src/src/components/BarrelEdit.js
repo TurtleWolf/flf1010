c1d81ee2d6738b7641b5f86e293c812a59130116
TurtleWolfe <jonpohlner@gmail.com>
2018-04-01 14:23:24 -0400

    <input type="text" class="datepicker"

--- client_src/src/components/BarrelEdit.js
+++ client_src/src/components/BarrelEdit.js
@@ -106,11 +106,11 @@ class EditBarrel extends Component {
                     <label htmlFor = "contents" >contents</label>
                 </div>
                 <div className = "input-field" >
-                    <input type = "date" name = "date_planted" ref = "date_planted" value = {this.state.date_planted} onChange = {this.handleInputChange} />
+                    <input type="text" class="datepicker" name = "date_planted" ref = "date_planted" value = {this.state.date_planted} onChange = {this.handleInputChange} />
                     <label htmlFor = "date_planted" ></label>
                 </div>
                 <div className = "input-field" >
-                    <input type = "date" name = "date_last_checked" ref = "date_last_checked"  value = {this.state.date_last_checked} onChange = {this.handleInputChange} />
+                    <input type="text" class="datepicker" name = "date_last_checked" ref = "date_last_checked"  value = {this.state.date_last_checked} onChange = {this.handleInputChange} />
                     <label htmlFor = "date_last_checked" ></label>
                 </div>
                 <div className = "input-field" >
